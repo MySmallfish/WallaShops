0f26c8450fa8966055660fe1d709ce988cdf5159
Yair Cohen <mysmallfish@gmail.com>
2013-12-16 17:34:45 +0200

    home page layout

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -2,7 +2,38 @@
 
     WS.HomeController = function ($scope) {
         $scope.name = "Yair";
-        $scope.promotion = "http://images1.calcalist.co.il/PicServer2/20122005/377794/AP0CAJH302_l.jpg";
+        $scope.main_promotion = {
+            promotion: "app/img/pic1.png",
+            name: "xyz"
+        };
+        
+        $scope.left_up = {
+            promotion: "app/img/pic3.png",
+            name: "xyz"
+        };
+        
+        $scope.left_down = {
+            promotion: "app/img/pic3.png",
+            name: "xyz"
+        };
+        
+        $scope.product = {
+            promotion: "app/img/product.png",
+            name: "xyz"
+        };
+
+        $scope.icon = {
+            promotion: "app/img/icon.png",
+            name: "xyz"
+        };
+
+        $scope.products = [
+            { id: 1, title: "מכונת גילוח", subtitle: "לגבר המטרוסקסואלי", rating: "123456", icons: [{ url: "app/img/icon.png" }, { url: "app/img/icon.png" }], imageUrl: "app/img/product.png", price: "1000" },
+            { id: 2, title: "מכונת גילוח", subtitle: "לגבר המטרוסקסואלי", rating: "123456", icons: [{ url: "app/img/icon.png" }], imageUrl: "app/img/product.png", price: "1000" },
+            { id: 3, title: "מכונת גילוח", subtitle: "לגבר המטרוסקסואלי", rating: "123456", icons: [{ url: "app/img/icon.png" }], imageUrl: "app/img/product.png", price: "1000" },
+            { id: 4, title: "מכונת גילוח", subtitle: "לגבר המטרוסקסואלי", rating: "123456", icons: [{ url: "app/img/icon.png" }], imageUrl: "app/img/product.png", price: "1000" },
+        ];
+
     };
 
 })(_, Simple, WallaShops);
