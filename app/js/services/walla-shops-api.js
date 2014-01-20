df576e7fe377eb34fec86d637d525673b4728996
MySmallfish <mysmallfish@gmail.com>
2014-01-20 09:01:18 +0200

    added api document

--- app/js/services/walla-shops-api.js
+++ app/js/services/walla-shops-api.js
@@ -2,100 +2,100 @@
 
     WS.WallaShopsApi = ["$q", function ($q) {
 
-        var categories = [
-     {
-         id: 1,
-         title: "קטגוריה 1",
-         level: 0,
-         categories: [
-             {
-                 parentId: 1,
-                 id: 4,
-                 title: "קטגורית משנה  11",
-                 level: 1,
-                 categories: [
-                     { id: 5, title: "קטגוריית משנה 2", level: 2 },
-                     { id: 6, title: "קטגוריית משנה 3", level: 2 },
-                     { id: 7, title: "קטגוריית משנה 4", level: 2 },
-                     { id: 8, title: "קטגוריית משנה 5", level: 2 },
-                     { id: 9, title: "קטגוריית משנה 6", level: 2 }
-                 ]
-             },
-             {
-                 parentId: 1,
-                 id: 9,
-                 title: "קטגורית משנה 11",
-                 level: 1,
-                 categories: [
-                     { id: 10, title: "קטגוריית משנה 2", level: 2 }
-                 ]
-             },
-             {
-                 id: 30,
-                 title: "google",
-                 extarnalLink: { url: "http://www.google.com" }
-             }
-         ]
-     },
-     {
-         id: 2,
-         title: "עוד קטגוריה",
-         level: 0,
-         categories: [
-             {
-                 parentId: 2,
-                 id: 11,
-                 title: "קטגורית משנה 2",
-                 level: 1,
-                 categories: [
-                     { id: 12, title: "קטגוריית משנה 2", level: 2 },
-                     { id: 13, title: "קטגוריית משנה 3", level: 2 },
-                     { id: 14, title: "קטגוריית משנה 4", level: 2 },
-                     { id: 15, title: "קטגוריית משנה 5", level: 2 },
-                     { id: 16, title: "קטגוריית משנה 6", level: 2 }
-                 ]
-             },
-             {
-                 parentId: 2,
-                 id: 9,
-                 title: "קטגורית משנה 2",
-                 level: 1,
-                 categories: [
-                     { id: 17, title: "קטגוריית משנה 2", level: 2 }
-                 ]
-             }
-         ]
-     },
-     {
-         id: 3,
-         title: "קטגוריה שלישת",
-         level: 0,
-         categories: [
-             {
-                 id: 24,
-                 parentId: 3,
-                 title: "קטגורית משנה 3",
-                 level: 1,
-                 categories: [
-                     { id: 18, title: "קטגוריית משנה 2", level: 2 },
-                     { id: 19, title: "קטגוריית משנה 3", level: 2 },
-                     { id: 20, title: "קטגוריית משנה 4", level: 2 },
-                     { id: 21, title: "קטגוריית משנה 5", level: 2 },
-                     { id: 22, title: "קטגוריית משנה 6", level: 2 }
-                 ]
-             },
-             {
-                 id: 25,
-                 parentId: 3,
-                 title: "3",
-                 level: 1,
-                 categories: [
-                     { id: 23, title: "קטגוריית משנה 2", level: 2 }
-                 ]
-             }
-         ]
-     }
-        ];
+     //   var categories = [
+     //{
+     //    id: 1,
+     //    title: "קטגוריה 1",
+     //    level: 0,
+     //    categories: [
+     //        {
+     //            parentId: 1,
+     //            id: 4,
+     //            title: "קטגורית משנה  11",
+     //            level: 1,
+     //            categories: [
+     //                { id: 5, title: "קטגוריית משנה 2", level: 2 },
+     //                { id: 6, title: "קטגוריית משנה 3", level: 2 },
+     //                { id: 7, title: "קטגוריית משנה 4", level: 2 },
+     //                { id: 8, title: "קטגוריית משנה 5", level: 2 },
+     //                { id: 9, title: "קטגוריית משנה 6", level: 2 }
+     //            ]
+     //        },
+     //        {
+     //            parentId: 1,
+     //            id: 9,
+     //            title: "קטגורית משנה 11",
+     //            level: 1,
+     //            categories: [
+     //                { id: 10, title: "קטגוריית משנה 2", level: 2 }
+     //            ]
+     //        },
+     //        {
+     //            id: 30,
+     //            title: "google",
+     //            extarnalLink: { url: "http://www.google.com" }
+     //        }
+     //    ]
+     //},
+     //{
+     //    id: 2,
+     //    title: "עוד קטגוריה",
+     //    level: 0,
+     //    categories: [
+     //        {
+     //            parentId: 2,
+     //            id: 11,
+     //            title: "קטגורית משנה 2",
+     //            level: 1,
+     //            categories: [
+     //                { id: 12, title: "קטגוריית משנה 2", level: 2 },
+     //                { id: 13, title: "קטגוריית משנה 3", level: 2 },
+     //                { id: 14, title: "קטגוריית משנה 4", level: 2 },
+     //                { id: 15, title: "קטגוריית משנה 5", level: 2 },
+     //                { id: 16, title: "קטגוריית משנה 6", level: 2 }
+     //            ]
+     //        },
+     //        {
+     //            parentId: 2,
+     //            id: 9,
+     //            title: "קטגורית משנה 2",
+     //            level: 1,
+     //            categories: [
+     //                { id: 17, title: "קטגוריית משנה 2", level: 2 }
+     //            ]
+     //        }
+     //    ]
+     //},
+     //{
+     //    id: 3,
+     //    title: "קטגוריה שלישת",
+     //    level: 0,
+     //    categories: [
+     //        {
+     //            id: 24,
+     //            parentId: 3,
+     //            title: "קטגורית משנה 3",
+     //            level: 1,
+     //            categories: [
+     //                { id: 18, title: "קטגוריית משנה 2", level: 2 },
+     //                { id: 19, title: "קטגוריית משנה 3", level: 2 },
+     //                { id: 20, title: "קטגוריית משנה 4", level: 2 },
+     //                { id: 21, title: "קטגוריית משנה 5", level: 2 },
+     //                { id: 22, title: "קטגוריית משנה 6", level: 2 }
+     //            ]
+     //        },
+     //        {
+     //            id: 25,
+     //            parentId: 3,
+     //            title: "3",
+     //            level: 1,
+     //            categories: [
+     //                { id: 23, title: "קטגוריית משנה 2", level: 2 }
+     //            ]
+     //        }
+     //    ]
+     //}
+     //   ];
 
         var filters = [
     {
@@ -224,12 +224,25 @@
                id: 2
            }];
 
+        var apiCategories = [{ "ID": 15, "MenuName": "מוצרי חשמל ואלקטרוניקה", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "http://10.0.2.129/bigmgr4/TabletIcons/electr.png", "DesignProps": { "IsBold": "false", "BackgroundColor": "008198#" }, "PositionDescription": "", "Menus": [{ "ID": 22, "MenuName": "טלוויזיה LCD", "MenuType": 4, "MainCategoryID": 21, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [{ "ID": 26, "MenuName": "טלוויזיה LCD", "MenuType": 4, "MainCategoryID": 21, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 387, "ImageTiming": null }, { "ID": 27, "MenuName": "טלוויזיה LED", "MenuType": 4, "MainCategoryID": 21, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "/bigmgr4/TabletIcons/", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 736, "ImageTiming": null }], "SubCategoryID": 387, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 14, "MenuName": "מחשבים ותקשורת", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "http://10.0.2.129/bigmgr4/TabletIcons/comput.png", "DesignProps": { "IsBold": "false", "BackgroundColor": "AD193D#" }, "PositionDescription": "", "Menus": [{ "ID": 19, "MenuName": "מחשבים ניידים וטאבלטים", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [{ "ID": 23, "MenuName": "אביזרים למחשב נייד", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 24, "MenuName": "מחשבי טאבלט ואביזרים", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 16, "MenuName": "רהיטים לבית ולגן", "MenuType": 3, "MainCategoryID": 17, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "http://10.0.2.129/bigmgr4/TabletIcons/furniture.pn", "DesignProps": { "IsBold": "false", "BackgroundColor": "CE4625#" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 17, "MenuName": "ספורט פנאי ומחנאות", "MenuType": 3, "MainCategoryID": 9, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "http://10.0.2.129/bigmgr4/TabletIcons/sport.png", "DesignProps": { "IsBold": "false", "BackgroundColor": "018B00#" }, "PositionDescription": "", "Menus": [{ "ID": 29, "MenuName": "ספורט פנאי ומחנאות", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "/bigmgr4/TabletIcons/sport.png", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [{ "ID": 31, "MenuName": "מסלול ריצה", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "/bigmgr4/TabletIcons/sport.png", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 30, "MenuName": "הלבשה והנעלה", "MenuType": 1, "MainCategoryID": 0, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "/bigmgr4/TabletIcons/sport.png", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 18, "MenuName": "דיל יומי", "MenuType": 3, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "http://10.0.2.129/bigmgr4/TabletIcons/more.png", "DesignProps": { "IsBold": "false", "BackgroundColor": "5134AB#" }, "PositionDescription": "", "Menus": [{ "ID": 21, "MenuName": "אלבומי תמונות", "MenuType": 2, "MainCategoryID": 0, "MenuLink": "http://wallashops.co.il/print/main.aspx", "IsNewWindow": false, "MenuIconPath": "blank.gif", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": -1, "ImageTiming": null }, { "ID": 28, "MenuName": "דיל יומי", "MenuType": 3, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "/bigmgr4/TabletIcons/more.png", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [{ "ID": 32, "MenuName": "מסעדות בתי קפה", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 851, "ImageTiming": null }, { "ID": 33, "MenuName": "אופנה טיפוח ובריאות", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 855, "ImageTiming": null }, { "ID": 34, "MenuName": "תיירות ונופש", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 854, "ImageTiming": null }, { "ID": 35, "MenuName": "אלקטרוניקה וגאדג'טים", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 860, "ImageTiming": null }, { "ID": 36, "MenuName": "רכב ופנאי", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 856, "ImageTiming": null }, { "ID": 37, "MenuName": "תרבות ואטרקציות", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 852, "ImageTiming": null }, { "ID": 38, "MenuName": "תינוקות וילדים", "MenuType": 4, "MainCategoryID": 43, "MenuLink": "", "IsNewWindow": false, "MenuIconPath": "", "DesignProps": { "IsBold": "true", "BackgroundColor": "" }, "PositionDescription": "", "Menus": [], "SubCategoryID": 849, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }], "SubCategoryID": -1, "ImageTiming": null }];
 
+        function mapMenuCategory(category, parent) {
+                return {
+                    id: category.ID,
+                    title: category.MenuName,
+                    icon: category.MenuIconPath,
+                    parentId: parent ? parent.ID : null,
+                    categories: _.map(category.Menus, function(subCategory) {
+                        return mapMenuCategory(subCategory, category);
+                    })
+                };
+        }
 
         function getCategories() {
 
             var result = $q.defer();
-
+            
+            var categories = _.map(apiCategories, mapMenuCategory);
             result.resolve(categories);
 
             return result.promise;
