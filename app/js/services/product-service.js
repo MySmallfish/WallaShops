d556f12d612d9f9dffeb4db2e5af7d5159450173
MySmallfish <mysmallfish@gmail.com>
2014-03-10 09:00:27 +0200

    images cache first shot

--- app/js/services/product-service.js
+++ app/js/services/product-service.js
@@ -1,12 +1,12 @@
 ﻿(function (_, S, WS) {
 
-    WS.ProductService = ["$q", "dailyCacheService", "wallaShopsApi", function ($q, dailyCacheService, wallaShopsApi) {
+    WS.ProductService = ["$q", "dailyCacheService", "wallaShopsApi", "remoteStorage", "fileManager", function ($q, dailyCacheService, wallaShopsApi, remoteStorage, fileManager) {
 
         function getOtherInterestedPromotionsCategories() {
 
             return wallaShopsApi.getOtherInterestedPromotionsCategories();
         }
-        
+
         function getBestSellersPromotionsCategories() {
 
             return wallaShopsApi.getBestSellersPromotionsCategories();
@@ -20,7 +20,7 @@
 
         function search(parameters) {
 
-            var result = 0;
+            var result = $q.when({});
 
             if (parameters.searchTerm) {
                 result = wallaShopsApi.getSearchProducts(parameters.searchTerm);
@@ -35,16 +35,48 @@
             }
 
 
-            return result;
+            return result.then(cacheProductsImages);
         }
 
+        function cacheProductImage(product) {
+            var directory = "ImagesCache",
+                fileName = product.id + ".jpg";
 
+            function setLocalImageUrl(fileEntry) {
+                if (fileEntry) {
+                    product.localImageUrl = fileEntry.toURL();
+                } else {
+                    product.localImageUrl = null;
+                }
+            }
 
+            function dowloadFile(fileEntry) {
+                remoteStorage.downloadFile({
+                    url: product.imageUrl,
+                    filePath: fileEntry.fullPath
+                }).then(setLocalImageUrl);
+            }
+
+            function downloadProductImage() {
+                return fileManager.getFile(directory, fileName).then(dowloadFile);
+            }
+
+            // reset
+            setLocalImageUrl();
+
+            fileManager.find(directory, fileName).then(setLocalImageUrl, downloadProductImage);
+
+        }
+
+        function cacheProductsImages(products) {
+            _.each(products, cacheProductImage);
+            return products;
+        }
 
         return {
             getOtherInterestedPromotionsCategories: getOtherInterestedPromotionsCategories,
             getBestSellersPromotionsCategories: getBestSellersPromotionsCategories,
-            getPromotionsCategories:getPromotionsCategories,
+            getPromotionsCategories: getPromotionsCategories,
             search: search
         };
 
