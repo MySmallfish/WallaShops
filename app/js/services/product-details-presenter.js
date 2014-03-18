d3200b7834d2a7f7f76fa1ccbd6758334d8abf7e
MySmallfish <mysmallfish@gmail.com>
2014-03-18 23:41:26 +0200

    Images cache, minor bug fixes

--- app/js/services/product-details-presenter.js
+++ app/js/services/product-details-presenter.js
@@ -8,7 +8,7 @@
             if (product.nameForUrl) {
                 analytics.recordClick(_.extend(WS.AnalyticsEvents.ShowProduct, { Category: product.nameForUrl }));
                 var productUri = encodeURI(wsUrl + (product.nameForUrl.indexOf("/") == 0 ? "" : "/") + product.nameForUrl);
-                window.open(productUri, "", "location=0,width=300,height=200");
+                window.open(productUri, "_blank", "location=0");
             }
         };
 
