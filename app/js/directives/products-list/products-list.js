2a0a336948e7a5b07e91e0c1b223355b63d6d4bb
MySmallfish <mysmallfish@gmail.com>
2014-05-12 08:40:13 +0300

    1.1.59
    updates

--- app/js/directives/products-list/products-list.js
+++ app/js/directives/products-list/products-list.js
@@ -7,34 +7,7 @@
             category: "=",
             canCheck: "="
         },
-        replace: true,
-        controller: ["$scope", "$filter", function ($scope, $filter) {
-            $scope.step = 4;
-            $scope.navigationInfo = {};
-            $scope.points = [];
-            
-            $scope.$watch("category", function () {
-                if ($scope.category) {
-                    $scope.productsListLength = $scope.category.products.length;
-                }
-            });
-            
-            function updateProductPage(navigationInfo) {
-                if ($scope.category) {
-                    $scope.currentProductsPage = $scope.category.products;
-                    $scope.productsListLength = $scope.category.products.length;
-                }
-            };
-            $scope.$watch("navigationInfo", function (newValue) {
-                updateProductPage(newValue);
-            });
-
-            updateProductPage({});
-        }],
-        link: function (scope, element, attributes, ctrl) {
-            // bind clicks...          
-
-        }
+        replace: true
     };
 
 
